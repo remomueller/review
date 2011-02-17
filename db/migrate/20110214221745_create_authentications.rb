df04165f2956874c51d57d1ee56cf4c85424dda5
Remo Mueller <remo.mueller@case.edu>
2011-02-17 09:57:23 -0500

    Fixed migration file.

--- db/migrate/20110214221745_create_authentications.rb
+++ db/migrate/20110214221745_create_authentications.rb
@@ -3,7 +3,7 @@ class CreateAuthentications < ActiveRecord::Migration
     create_table :authentications do |t|
       t.integer :user_id
       t.string :provider
-      t.uid :string
+      t.string :uid
       
       t.timestamps
     end
