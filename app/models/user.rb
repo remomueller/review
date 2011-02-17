e8feccae930a454b1a58ebd1a7579e3281f730f6
Remo Mueller <remo.mueller@case.edu>
2011-02-17 17:56:12 -0500

    Fixes.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ActiveRecord::Base
-  has_many :authentications
-  
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
   devise :database_authenticatable, :registerable,
@@ -8,4 +6,31 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
+  
+  # Model Validation
+  validates_presence_of     :first_name
+  validates_presence_of     :last_name
+
+  # Model Relationships
+  has_many :authentications
+    
+
+  def name
+    "#{first_name} #{last_name}"
+  end
+  
+  def rev_name
+    "#{last_name}, #{first_name}"
+  end
+  
+  def apply_omniauth(omniauth)
+    self.email = omniauth['user_info']['email'] if email.blank?
+    self.first_name =  omniauth['user_info']['first_name'] if first_name.blank?
+    self.last_name =  omniauth['user_info']['last_name'] if last_name.blank?
+    authentications.build(:provider => omniauth['provider'], :uid => omniauth['uid'])
+  end
+
+  def password_required?
+    (authentications.empty? || !password.blank?) && super
+  end
 end
