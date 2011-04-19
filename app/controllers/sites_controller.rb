9566a7936194474edcbf852c68f42514c9a4deea
Remo Mueller <remosm@gmail.com>
2011-04-19 17:49:17 -0400

    Dashboard has been removed, and publication matrix is now the front interface.  Publication matrix search bar bumped up too.

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -5,7 +5,4 @@ class SitesController < ApplicationController
     
   end
   
-  def dashboard
-    
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -91,7 +91,7 @@ Review::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "sites#dashboard"
+  root :to => "publications#index"
 
   # See how all your routes lay out with "rake routes"
 
