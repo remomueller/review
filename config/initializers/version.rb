4e5ce822bde4df0eb8a8ef4abb09e29ebc71786d
Remo Mueller <remosm@gmail.com>
2011-08-15 13:31:04 -0400

    Version file updated.

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -4,6 +4,7 @@ module Review
     MINOR = 8
     TINY = 0
     BUILD = "pre" # nil, "pre", "beta1", "beta2", "rc", "rc2"
+    
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
   end
 end
