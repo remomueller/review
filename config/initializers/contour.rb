4c2d29b3832a1979a969f8c2340fbc5a0231fef6
Remo Mueller <remosm@gmail.com>
2011-09-07 10:04:41 -0400

    Contour finalized and application layout complete.

--- config/initializers/contour.rb
+++ config/initializers/contour.rb
@@ -3,10 +3,15 @@ Contour.setup do |config|
   
   # Enter your application name here. The name will be displayed in the title of all pages, ex: AppName - PageTitle
   config.application_name = DEFAULT_APP_NAME
-  # '<span style="color:#D4002F">C</span><span style="color:#56DF00">H</span><span style="color:#D9DE00">A</span><span style="color:#23FFFF">T</span> <span style="color:#2F0C72">Publications</span>'
+  
+  # If you want to style your name using html you can do so here, ex: <b>App</b>Name
+  config.application_name_html = '<span style="color:#D4002F">C</span><span style="color:#56DF00">H</span><span style="color:#D9DE00">A</span><span style="color:#23FFFF">T</span> <span style="color:#2F0C72">Publications</span>'
   
   # Enter your application version here. Do not include a trailing backslash. Recommend using a predefined constant
   config.application_version = Review::VERSION::STRING
+
+  # Enter the url of your site without a trailing slash, ex: http://localhost/app_root
+  config.application_site_url = SITE_URL
   
   # Enter your application header background image here.
   config.header_background_image = 'brigham.png'
@@ -31,8 +36,8 @@ Contour.setup do |config|
                {:name => 'About', :path => 'about_path'}]
   },
   {
-    :name => 'Manual', :id => 'manual', :display => 'not_signed_in', :position => 'left', :position_class => 'left_center',
-    :links => [{:name => 'Manual', :path => 'SITE_URL + \'/documents/CHAT Publications Manual.pdf\'', :target => '_blank', :image => "image_tag('contour/pdf.png', :style => 'vertical-align:middle')"}]
+    :name => 'Manual ', :id => 'manual', :display => 'not_signed_in', :position => 'left', :position_class => 'left_center', :image => 'contour/pdf.png', :image_options => {:style => 'vertical-align:middle'},
+    :links => [{:name => 'Manual ', :path => 'SITE_URL + \'/documents/CHAT Publications Manual.pdf\'', :target => '_blank', :image => 'contour/pdf.png', :image_options => {:style => 'vertical-align:middle'}}]
   },
   {
     :name => 'Users', :id => 'users', :display => 'signed_in', :position => 'left', :position_class => 'left_center',
