1ada94d3b6e3269e93cc10c9777d06f19049ea5b
Remo Mueller <remosm@gmail.com>
2012-05-02 12:46:18 -0400

    Add auth token to reviewer email reminder for automated login

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,32 +1,33 @@
 class UserMailer < ActionMailer::Base
   default :from => ActionMailer::Base.smtp_settings[:user_name]
-  
+
   def notify_system_admin(system_admin, user)
     setup_email
     @system_admin = system_admin
     @user = user
-    mail(:to => system_admin.email,
-         :subject => @subject + "#{user.name} Signed Up",
-         :reply_to => user.email)
+    mail(to: system_admin.email,
+         subject: @subject + "#{user.name} Signed Up",
+         reply_to: user.email)
   end
-  
+
   def publication_approval_reminder(publication, reviewer)
     setup_email
     @publication = publication
     @reviewer = reviewer
-    mail(:to => reviewer.email,
-         :subject => @subject + "New Publication Awaiting Approval: #{publication.abbreviated_title_and_ms}",
-         :reply_to => publication.user.email)
+    @reviewer.reset_authentication_token!
+    mail(to: reviewer.email,
+         subject: @subject + "New Publication Awaiting Approval: #{publication.abbreviated_title_and_ms}",
+         reply_to: publication.user.email)
   end
-  
+
   def status_activated(user)
     setup_email
     @user = user
-    mail(:to => user.email,
-         :subject => @subject + "#{user.name}'s Account Activated") #,
-#         :reply_to => user.email)
+    mail(to: user.email,
+         subject: @subject + "#{user.name}'s Account Activated") #,
+#         reply_to: user.email)
   end
-  
+
   def publication_approval(publication, pp_committee, secretary)
     setup_email
     @publication = publication
@@ -42,28 +43,28 @@ class UserMailer < ActionMailer::Base
     elsif @publication.status == 'not approved' and not pp_committee
       @approval_status = 'denied by the Steering Committee'
     end
-    mail(:to => @user.email,
-         :subject => @subject + "Publication Proposal for #{@publication.abbreviated_title_and_ms} has been #{@approval_status}",
-         :reply_to => secretary.email)
+    mail(to: @user.email,
+         subject: @subject + "Publication Proposal for #{@publication.abbreviated_title_and_ms} has been #{@approval_status}",
+         reply_to: secretary.email)
   end
-  
+
   def review_updated(user_publication_review, secretary)
     setup_email
     @secretary = secretary
     @user_publication_review = user_publication_review
     @reviewer = user_publication_review.user
     @publication = user_publication_review.publication
-    
-    mail(:to => secretary.email,
-         :subject => @subject + "#{@reviewer.name} has reviewed #{@publication.abbreviated_title_and_ms}",
-         :reply_to => @reviewer.email
+
+    mail(to: secretary.email,
+         subject: @subject + "#{@reviewer.name} has reviewed #{@publication.abbreviated_title_and_ms}",
+         reply_to: @reviewer.email
     )
   end
-  
+
   protected
-  
+
   def setup_email
     @subject = "[#{DEFAULT_APP_NAME.downcase}#{'-development' if Rails.env == 'development'}] "
   end
-  
+
 end
