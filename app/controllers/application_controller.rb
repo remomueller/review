06bce5f466551f17aedf108a8fd35738c6fb8b1d
Remo Mueller <remosm@gmail.com>
2013-03-20 13:14:52 -0400

    Added configurable fallback routing for html/js/json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,4 +20,13 @@ class ApplicationController < ActionController::Base
   def check_system_admin
     redirect_to root_path, alert: "You do not have sufficient privileges to access that page." unless current_user.system_admin?
   end
+
+  def empty_response_or_root_path(path = root_path)
+    respond_to do |format|
+      format.html { redirect_to path }
+      format.js { render nothing: true }
+      format.json { head :no_content }
+    end
+  end
+
 end
