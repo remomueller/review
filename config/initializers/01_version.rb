b022a100c39392bdeca0f6b7f6524532a013365c
Remo Mueller <remosm@gmail.com>
2016-01-26 15:10:03 -0500

    Version bump to 0.14.17.pre

--- config/initializers/01_version.rb
+++ config/initializers/01_version.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 module Review
   module VERSION #:nodoc:
     MAJOR = 0
     MINOR = 14
-    TINY = 16
-    BUILD = nil # nil, "pre", "beta1", "beta2", "rc", "rc2"
+    TINY = 17
+    BUILD = 'pre'.freeze # nil, "pre", "beta1", "beta2", "rc", "rc2"
 
-    STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
+    STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.').freeze
   end
 end
