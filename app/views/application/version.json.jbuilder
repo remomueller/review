58aaaf6cd9680ff3bf2d7c22947322cdf5a3f2d3
Remo Mueller <remosm@gmail.com>
2016-01-26 15:18:09 -0500

    Added missing MINOR attribute to version API

--- app/views/application/version.json.jbuilder
+++ app/views/application/version.json.jbuilder
@@ -1,6 +1,7 @@
 json.version do
   json.string Review::VERSION::STRING
   json.major Review::VERSION::MAJOR
+  json.minor Review::VERSION::MINOR
   json.tiny Review::VERSION::TINY
   json.build Review::VERSION::BUILD
 end
--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -1,18 +1,24 @@
 require 'test_helper'
 
 class ApplicationControllerTest < ActionController::TestCase
-  setup do
-    # Nothing
-  end
-
-  test "should get about" do
+  test 'should get about' do
     get :about
     assert_response :success
   end
 
-  test "should get version" do
+  test 'should get version' do
     get :version
     assert_response :success
   end
 
+  test 'should get version as json' do
+    get :version, format: 'json'
+    version = JSON.parse(response.body)
+    assert_equal Review::VERSION::STRING, version['version']['string']
+    assert_equal Review::VERSION::MAJOR, version['version']['major']
+    assert_equal Review::VERSION::MINOR, version['version']['minor']
+    assert_equal Review::VERSION::TINY, version['version']['tiny']
+    assert_equal Review::VERSION::BUILD, version['version']['build']
+    assert_response :success
+  end
 end
